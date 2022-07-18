aadd4f3af821364b28145ba3b8c73009c6ba32e3
kelvin-AJ <aji22001@byui.edu>
2022-07-18 13:07:46 +0100

    TEMPLE PAGE COMMIT #2

---
+++ js/temple.js
@@ -0,0 +1,95 @@
+const url = "https://kelvin-aj.github.io/theTempleInn/json/temples.json";
+const pageHeader = document.querySelector(".page-header");
+const pageOverlay = document.querySelector(".temples-overlay");
+const templesGrid = document.querySelector(".temples-grid");
+const templesLarge = document.querySelector(".temples-expanded");
+let templesArr = [];
+
+// LIKE TEMPLE
+const likeTemple = function(id){
+    const imgStatus = localStorage.getItem(`${id}`);
+    if(imgStatus == "not-liked") localStorage.setItem(`${id}`, "liked");
+    else localStorage.setItem(`${id}`, "not-liked");
+    displayTemples(templesArr);
+}
+
+// DISPLAY LARGE CARD
+const displayLarge = function(id){
+    const temple = templesArr[id - 1];
+    const status = localStorage.getItem(`${temple.id}`);
+    templesLarge.innerHTML ="";
+    templesLarge.innerHTML =
+    `
+    <div class="temple-temple-card temple-large-card">
+    <h2>${temple.name}</h2><button onclick="closeLarge()" class="close-temple-btn"><span>&#10005;</span></button>
+    <img class="temple-card-img" src="images/salt-lake.jpg" alt="${temple.name}">
+    <div class="temple-long-info">
+        <span>${temple.address}</span>
+        <span>${temple.telephone}</span>
+        <span><h3>Services:</h3><p>${temple.services}</p></span>
+        <span><h3>History:</h3><p>${temple.history}</p></span>
+        <span><h3>Ordinances:</h3><p>${temple.ordinances}</p></span>
+        <span><h3>Schedule:</h3>
+        <ul>
+            <li>${temple.schedule1}</li>
+            <li>${temple.schedule2}</li>
+        </ul>
+        </span>
+    </div>
+    <div class="temple-card-action-grid">
+    <img src="images/${status}-icon.png" alt="like button">
+    </div>
+</div>
+    `;
+    pageOverlay.classList.remove("hidden-all");
+};
+// CLOSE LARGE CARD
+const closeLarge = function(){
+    templesLarge.innerHTML ="";
+    pageOverlay.classList.add("hidden-all");
+}
+
+
+
+const displayTemples = function(temples){
+    temples.forEach(temple => {
+        if(localStorage.getItem("visited") != "true"){
+            localStorage.setItem(`${temple.id}`, "not-liked")
+        }
+    });
+
+    templesGrid.innerHTML = "";
+    temples.forEach(temple => {
+        const status = localStorage.getItem(`${temple.id}`);
+
+        //DISPLAY TEMPLE CARD
+        templesGrid.insertAdjacentHTML("beforeend", 
+        `<div class="temple-temple-card"  ondblclick="likeTemple(${temple.id})">
+        <h2>${temple.name}</h2>
+        <img src="images/salt-lake.jpg" alt="pictur of ${temple.name}">
+        <div class="temple-short-info">
+            <span>${temple.address}</span>
+            <span${temple.telephone}</span>
+            <span>${temples.email}</span>
+            <span onclick="displayLarge(${temple.id})"><b>more...</b></span>
+        </div>
+        <div class="temple-card-action-grid" onclick="likeTemple(${temple.id})">
+            <img src="images/${status}-icon.png" alt="like button">
+        </div>
+    </div>
+        `)
+    });
+}
+
+
+
+async function getTemples(){
+    const templesFetch = await fetch(url);
+    const json = await templesFetch.json();
+    console.log(json.temples[1]);
+    templesArr = json.temples;
+    displayTemples(json.temples);
+}
+getTemples()
+
+localStorage.setItem("visited", "true");
